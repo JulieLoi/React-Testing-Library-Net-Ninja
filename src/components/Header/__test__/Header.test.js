c0c1daf40a2ca2ed528fa111b1b07d3a8eeb27a2
JulieLoi <113395605+JulieLoi@users.noreply.github.com>
2023-02-07 20:45:01 -0800

    Describe blocks

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -6,52 +6,58 @@ describe("HEADER", () => {
 
     const titleText = "MY HEADER";
 
-    // Unit Test: getByText
-    test('should render same text passed into title prop - getByText', () => {
-        render(<Header title={titleText} />);
-        const headingElement = screen.getByText(new RegExp(titleText, "i"));     // /MY HEADER/i
-        expect(headingElement).toBeInTheDocument();
+    describe("Render Text, Different Query Types", () => {
+        // Unit Test: getByText
+        test('should render same text passed into title prop - getByText', () => {
+            render(<Header title={titleText} />);
+            const headingElement = screen.getByText(new RegExp(titleText, "i"));     // /MY HEADER/i
+            expect(headingElement).toBeInTheDocument();
+        });
+
+        // Unit Test: getByRole (specifies which header based on object)
+        test('should render same text passed into title prop - getByRole', () => {
+            render(<Header title={titleText} />);
+            const headingElement = screen.getByRole("heading", { name: titleText });     // h1
+            expect(headingElement).toBeInTheDocument();
+        });
+
+        // Unit Test: getByTitle (Semantic Query)
+        test('should render same text passed into title prop - getByTitle (semantic query)', () => {
+            render(<Header title={titleText} />);
+            const headingElement = screen.getByTitle("header-title");
+            expect(headingElement).toBeInTheDocument();
+        });
+
+        // Unit Test: getByTestId (Test ID)
+        test('should render same text passed into title prop - getByTestId (test id)', () => {
+            render(<Header title={titleText} />);
+            const headingElement = screen.getByTestId("header-test-id");
+            expect(headingElement).toBeInTheDocument();
+        });
     });
 
-    // Unit Test: getByRole (specifies which header based on object)
-    test('should render same text passed into title prop - getByRole', () => {
-        render(<Header title={titleText} />);
-        const headingElement = screen.getByRole("heading", { name: titleText });     // h1
-        expect(headingElement).toBeInTheDocument();
-    });
+    describe("Render Text, Different prefix (find, query", () => {
+        // Unit Test: findByText (asynchronous)
+        test('should render same text passed into title prop - findByText (async)', async () => {
+            render(<Header title={titleText} />);
+            const headingElement = await screen.findByText(new RegExp(titleText, "i"));     // /MY HEADER/i
+            expect(headingElement).toBeInTheDocument();
+        });
 
-    // Unit Test: getByTitle (Semantic Query)
-    test('should render same text passed into title prop - getByTitle (semantic query)', () => {
-        render(<Header title={titleText} />);
-        const headingElement = screen.getByTitle("header-title");
-        expect(headingElement).toBeInTheDocument();
-    });
+        // Unit Test: queryByText (asynchronous)
+        test('should render same text passed into title prop - queryByText (different text, not in document)', () => {
+            render(<Header title={titleText} />);
+            const headingElement = screen.queryByText(new RegExp("failtext", "i"));     // /MY HEADER/i
+            expect(headingElement).not.toBeInTheDocument();
+        });
+        })
 
-    // Unit Test: getByTestId (Test ID)
-    test('should render same text passed into title prop - getByTestId (test id)', () => {
-        render(<Header title={titleText} />);
-        const headingElement = screen.getByTestId("header-test-id");
-        expect(headingElement).toBeInTheDocument();
-    });
-
-    // Unit Test: findByText (asynchronous)
-    test('should render same text passed into title prop - findByText (async)', async () => {
-        render(<Header title={titleText} />);
-        const headingElement = await screen.findByText(new RegExp(titleText, "i"));     // /MY HEADER/i
-        expect(headingElement).toBeInTheDocument();
-    });
-
-    // Unit Test: queryByText (asynchronous)
-    test('should render same text passed into title prop - queryByText (different text, not in document)', () => {
-        render(<Header title={titleText} />);
-        const headingElement = screen.queryByText(new RegExp("failtext", "i"));     // /MY HEADER/i
-        expect(headingElement).not.toBeInTheDocument();
-    });
-
-    // Unit Test: getAllByRole
-    test('should render same text passed into title prop - getAllByText (array result)', () => {
-        render(<Header title={titleText} />);
-        const headingElement = screen.getAllByRole("heading", { name: titleText });     // h1
-        expect(headingElement.length).toBe(1);
-    });
+    describe("Render Text, Match Multiple (All)", () => {
+        // Unit Test: getAllByRole
+        test('should render same text passed into title prop - getAllByText (array result)', () => {
+            render(<Header title={titleText} />);
+            const headingElement = screen.getAllByRole("heading", { name: titleText });     // h1
+            expect(headingElement.length).toBe(1);
+        });
+    })
 });
--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import TodoFooter from '../TodoFooter';
 
-describe.only("TODO FOOTER", () => {
+describe("TODO FOOTER", () => {
 
     const taskNumber = 0;
     const paragraphText = `${taskNumber} ${taskNumber !== 1 ? "tasks" : "task"} left`
@@ -16,53 +16,57 @@ describe.only("TODO FOOTER", () => {
         );
     }
 
-    // Unit Test: Render `taskNumber` text
-    test("should render the correct amount of incomplete tasks (adjustable)", () => {
-        render(<MockTodoFooter numberOfIncompleteTasks={taskNumber} />);
-        const paragraphElement = screen.getByText(new RegExp(paragraphText, "i"));      // x tasks left (x !== 1)
-        expect(paragraphElement).toBeInTheDocument();
+    describe("Testing Render", () => {
+        // Unit Test: Render `taskNumber` text
+        test("should render the correct amount of incomplete tasks (adjustable)", () => {
+            render(<MockTodoFooter numberOfIncompleteTasks={taskNumber} />);
+            const paragraphElement = screen.getByText(new RegExp(paragraphText, "i"));      // x tasks left (x !== 1)
+            expect(paragraphElement).toBeInTheDocument();
+        });
+
+        // Unit Test: Render `1 task` text
+        test("should render the correct amount of incomplete tasks - 1 task", () => {
+            render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+            const paragraphElement = screen.getByText(/1 task left/i);                      // 1 task left 
+            expect(paragraphElement).toBeInTheDocument();
+        });
     });
 
-    // Unit Test: Render `1 task` text
-    test("should render the correct amount of incomplete tasks - 1 task", () => {
-        render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-        const paragraphElement = screen.getByText(/1 task left/i);                      // 1 task left 
-        expect(paragraphElement).toBeInTheDocument();
-    });
+    describe("Different Assertions", () => {
+        // Unit Test: Assertion (toBeVisible)
+        test("toBeVisible - check visibility", () => {
+            render(<MockTodoFooter numberOfIncompleteTasks={taskNumber} />);
+            const paragraphElement = screen.getByText(new RegExp(paragraphText, "i"));    
+            expect(paragraphElement).toBeVisible();
+        });
 
-    // Unit Test: Assertion (toBeVisible)
-    test("should render the correct amount of incomplete tasks - assertion (toBeVisible)", () => {
-        render(<MockTodoFooter numberOfIncompleteTasks={taskNumber} />);
-        const paragraphElement = screen.getByText(new RegExp(paragraphText, "i"));    
-        expect(paragraphElement).toBeVisible();
-    });
+        // Unit Test: Assertion (toContainHTML) - tag (<p>)
+        test("toContainHTML - check tag (<p></p>)", () => {
+            render(<MockTodoFooter numberOfIncompleteTasks={taskNumber} />);
+            const paragraphElement = screen.getByText(new RegExp(paragraphText, "i"));    
+            expect(paragraphElement).toContainHTML("p");
+        });
 
-    // Unit Test: Assertion (toContainHTML) - tag (<p>)
-    test("should render the correct amount of incomplete tasks - assertion (toContainHTML)", () => {
-        render(<MockTodoFooter numberOfIncompleteTasks={taskNumber} />);
-        const paragraphElement = screen.getByText(new RegExp(paragraphText, "i"));    
-        expect(paragraphElement).toContainHTML("p");
-    });
+        // Unit Test: Assertion (toHaveTextContent)
+        test("toHaveTextContent - check element's text content", () => {
+            render(<MockTodoFooter numberOfIncompleteTasks={taskNumber} />);
+            const paragraphElement = screen.getByText(new RegExp(paragraphText, "i"));    
+            expect(paragraphElement).toHaveTextContent(new RegExp(paragraphText, "i"));
+        });
 
-    // Unit Test: Assertion (toHaveTextContent)
-    test("should render the correct amount of incomplete tasks - assertion (toHaveTextContent)", () => {
-        render(<MockTodoFooter numberOfIncompleteTasks={taskNumber} />);
-        const paragraphElement = screen.getByText(new RegExp(paragraphText, "i"));    
-        expect(paragraphElement).toHaveTextContent(new RegExp(paragraphText, "i"));
-    });
+        // Unit Test: Assertion (not.toBeFalsy)
+        test("not.toBeFalsy - demonstrates `not`, checks for false", () => {
+            render(<MockTodoFooter numberOfIncompleteTasks={taskNumber} />);
+            const paragraphElement = screen.getByText(new RegExp(paragraphText, "i"));    
+            expect(paragraphElement).not.toBeFalsy();
+        });
 
-    // Unit Test: Assertion (not.toBeFalsy)
-    test("should render the correct amount of incomplete tasks - assertion (not.toBeFalsy)", () => {
-        render(<MockTodoFooter numberOfIncompleteTasks={taskNumber} />);
-        const paragraphElement = screen.getByText(new RegExp(paragraphText, "i"));    
-        expect(paragraphElement).not.toBeFalsy();
-    });
-
-    // Unit Test: Assertion (toBe)
-    test("should render the correct amount of incomplete tasks - assertion, get value (toBe)", () => {
-        render(<MockTodoFooter numberOfIncompleteTasks={taskNumber} />);
-        const paragraphElement = screen.getByText(new RegExp(paragraphText, "i"));    
-        expect(paragraphElement.textContent).toBe(paragraphText);
+        // Unit Test: Assertion (toBe)
+        test("toBe - check text content equality", () => {
+            render(<MockTodoFooter numberOfIncompleteTasks={taskNumber} />);
+            const paragraphElement = screen.getByText(new RegExp(paragraphText, "i"));    
+            expect(paragraphElement.textContent).toBe(paragraphText);
+        });
     });
 
 });
